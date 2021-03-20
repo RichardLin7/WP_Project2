a46634dcfa84d1e180185c324f3976180e4ff7e2
Richard Lin <rlin7@student.gsu.edu>
2021-03-20 14:49:11 -0400

    Files Created
    Created home.php and jeopardy.css

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Jeopardy Homepage</title>
+        <link rel="stylesheet" href="./jeopardy.css"/>
+    </head>
+    <body>
+        <div class="center">
+            <h1>Jeopardy</h1>
+        </div>
+
+        <div class="columns">
+            <!-- Leader Board -->
+            <div class="leftcol col">
+                <h3>Leaderboard</h3>
+                <p>
+                    This is where the leaderboard code goes.
+                </p>
+            </div>
+  
+            <!-- Question Board -->
+            <div class="rightcol col">
+                <h3>Jeopardy Question Board/h3>
+                <p>
+                    This is where the question table goes.
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
