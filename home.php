2585778a57594ae25328dd4a53b1b5a84ff7d0cf
Richard Lin <rlin7@student.gsu.edu>
2021-03-20 16:00:00 -0400

    updated home.php, jeopardy.css
    Implemented turn indicator (telling who's turn it is), created questions table, added/reformatted text. Set up links for questions table.

--- home.php
+++ home.php
@@ -5,13 +5,19 @@
         <link rel="stylesheet" href="./jeopardy.css"/>
     </head>
     <body>
-        <div class="center">
-            <h1>Jeopardy</h1>
-        </div>
+        <?php $name = $_POST["name"];?>
 
+        <div class="center">
+            <h1>WEB PROGRAMMING JEOPARDY</h1>
+        </div>
         <div class="columns">
             <!-- Leader Board -->
             <div class="leftcol col">
+                <p>
+                    It's <?php print $name ?> turn!
+                    </br>
+                    _________________________
+                </p>
                 <h3>Leaderboard</h3>
                 <p>
                     This is where the leaderboard code goes.
@@ -20,10 +26,50 @@
   
             <!-- Question Board -->
             <div class="rightcol col">
-                <h3>Jeopardy Question Board/h3>
-                <p>
-                    This is where the question table goes.
-                </p>
+            <table>
+                <tr>
+                    <th>Random Subjects</th> 
+                    <th>Computer Science</th>
+                    <th>Memes</th>
+                    <th>Big Brain Questions</th>
+                    <th>Smol Brain Questions</th>    
+                </tr>
+                <tr>
+                    <td><a href = "scene1.html">100</a></td>
+                    <td><a href = "scene1.html">100</a></td>
+                    <td><a href = "scene1.html">100</a></td>
+                    <td><a href = "scene1.html">100</a></td>
+                    <td><a href = "scene1.html">100</a></td> 
+                </tr>
+                <tr>
+                    <td><a href = "scene1.html">200</a></td>
+                    <td><a href = "scene1.html">200</a></td>
+                    <td><a href = "scene1.html">200</a></td>
+                    <td><a href = "scene1.html">200</a></td>
+                    <td><a href = "scene1.html">200</a></td>
+                </tr>
+                <tr>
+                    <td><a href = "scene1.html">300</a></td>
+                    <td><a href = "scene1.html">300</a></td>
+                    <td><a href = "scene1.html">300</a></td>
+                    <td><a href = "scene1.html">300</a></td>
+                    <td><a href = "scene1.html">300</a></td>
+                </tr>
+                <tr>
+                    <td><a href = "scene1.html">400</a></td>
+                    <td><a href = "scene1.html">400</a></td>
+                    <td><a href = "scene1.html">400</a></td>
+                    <td><a href = "scene1.html">400</a></td>
+                    <td><a href = "scene1.html">400</a></td>
+                </tr>
+                <tr>
+                    <td><a href = "scene1.html">500</a></td>
+                    <td><a href = "scene1.html">500</a></td>
+                    <td><a href = "scene1.html">500</a></td>
+                    <td><a href = "scene1.html">500</a></td>
+                    <td><a href = "scene1.html">500</a></td>
+                </tr>
+            </table>
             </div>
         </div>
     </body>
