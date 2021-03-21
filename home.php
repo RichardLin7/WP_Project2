9c6dbb1a09052ee92bd875c46203954c167a43ba
Richard Lin <rlin7@student.gsu.edu>
2021-03-20 22:18:20 -0400

    Updated home.php, jeopardy.css
    Implemented Yuhan's work

--- home.php
+++ home.php
@@ -11,10 +11,13 @@
             $player2 = $_POST["player2"]; // variables subject to change for leaderboard
             $player3 = $_POST["player3"]; // variables subject to change for leaderboard
             $player4 = $_POST["player4"]; // variables subject to change for leaderboard
-
-
         ?>
-
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="signup.php">Register</a>
+            <a href="login.php">Login</a>
+            <a href="about.php">About Us</a>
+        </div>
         <div class="center">
             <h1>WEB PROGRAMMING JEOPARDY</h1>
         </div>
