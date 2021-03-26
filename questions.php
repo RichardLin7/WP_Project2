0de5b199ac09a7b2fd12dd7c7aea87a158425345
Yuhan Yang <55200109+yune-lilias@users.noreply.github.com>
2021-03-25 22:45:46 -0400

    Add maths question, modified outdated reference

--- questions.php
+++ questions.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <div class="topnav">
-            <a class="active" href="#home">Home</a>
+            <a class="active" href="index.php">Home</a>
             <a href="signup.php">Register</a>
             <a href="login.php">Login</a>
             <a href="about.php">About Us</a>
@@ -35,7 +35,7 @@
                 $file = "./questions.txt";
                 $contents = file_get_contents($file);
                 $subject = $_POST["subject"];
-                $pattern = "/".$subject."[,](.*?)[,]what is(.*?)[?]/i";
+                $pattern = "/".$subject."[,](.*?)[,](.*?)[,]/i";
                 preg_match($pattern,$contents,$match);
                 $array = preg_split("/\,/", $match[0]);
                 setcookie("subject", $array[0], time() + (3600 * 30), "/");
